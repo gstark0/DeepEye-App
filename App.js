b6449551a60a97f8af9d840609636ebad23bffc3
gstark0 <gstark0@icloud.com>
2018-12-21 22:41:52 +0100

    Camera & API requests implemented

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableHighlight } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	state = {
+		hasCameraPermission: null,
+		type: Camera.Constants.Type.back,
+	};
+
+	async componentDidMount() {
+		const { status } = await Permissions.askAsync(Permissions.CAMERA);
+		this.setState({ hasCameraPermission: status === 'granted' });
+	}
+
+	takePhoto = async () => {
+		if (this.camera) {
+			let photo = await this.camera.takePictureAsync({skipProcessing: true, autoFocus: false, base64: true});
+			fetch('http://192.168.0.192:5000/recognize', {
+				method: 'POST',
+				body: JSON.stringify({
+					image: photo.base64
+				})
+			})
+			.then(resp => resp.json())
+			.then(resp => {
+				console.warn(resp);
+			})
+		}
+	}
+
+	render() {
+		const { hasCameraPermission } = this.state;
+		if(hasCameraPermission == null) {
+			return <View />;
+		} else if (hasCameraPermission == false) {
+			return (
+				<View>
+					<Text>No access to camera!</Text>
+				</View>
+			);
+		} else {
+			return (
+				<Camera ref={ref => { this.camera = ref; }} style={styles.camera}>
+					<TouchableHighlight title='Take' style={styles.takePhotoButton} onPress={this.takePhoto}>
+						<Text></Text>
+					</TouchableHighlight>
+				</Camera>
+			);
+		}
+	}
 }
 
+const {height, width} = Dimensions.get('window');
+const newWidth = height*(3/4);
+const widthOffset = -((newWidth-width)/2);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+
+	camera: {
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		right: 0,
+		left: 0,
+		zIndex: 2,
+		left: widthOffset,
+		right: widthOffset,
+	},
+
+	takePhotoButton: {
+		position: 'absolute',
+		backgroundColor: 'white',
+		width: 80,
+		height: 80,
+		bottom: '5%',
+		margin: 'auto',
+		alignSelf: 'center',
+		borderRadius: 100
+	}
 });
